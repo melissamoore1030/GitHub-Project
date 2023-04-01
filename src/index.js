274c771a3c4bbffce6ee1206f237e1b5baf0c46f
melissamoore1030 <128547771+melissamoore1030@users.noreply.github.com>
2023-04-01 17:57:10 -0400

    Added C/F functions, wind, description, icon, and conversions

--- src/index.js
+++ src/index.js
@@ -37,7 +37,17 @@ form.addEventListener("submit", showCity);
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = `<strong>${temperature}°</strong>`;
+  temperatureElement.innerHTML = `<strong>${temperature}</strong>`;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  celsiusTemperature = response.data.main.temp;
 }
 
 function searchTemp(event) {
@@ -54,9 +64,19 @@ searchForm.addEventListener("submit", searchTemp);
 function showLocationTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let locationTemp = document.querySelector("#current-temp");
-  locationTemp.innerHTML = `<strong>${temperature}°</strong>`;
+  locationTemp.innerHTML = `<strong>${temperature}</strong>`;
   let city = document.querySelector("#city-now");
   city.innerHTML = `<strong>${response.data.name}<strong>`;
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.weather[0].description;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  celsiusTemperature = response.data.main.temp;
 }
 
 function showPosition(position) {
@@ -75,3 +95,30 @@ function getCurrentPosition() {
 
 let locationButton = document.querySelector("#location-temp");
 locationButton.addEventListener("click", getCurrentPosition);
+
+function showFahrenheit(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#current-temp");
+  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+function showCelsius(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#current-temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", showCelsius);
+
+let celsiusTemperature = null;
+
+searchTemp("Albany");
